32c1032dd8f46872bbbf6de0e9c78b5fbaab289f
stasson <sebastien.tasson@gmail.com>
2018-09-01 19:49:20 +0200

    chore: add formatting unit test

---
+++ test/formatmsg.spec.ts
@@ -0,0 +1,80 @@
+import { formatMessage } from './lib'
+
+const loreipsum =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+describe('formatMessage', () => {
+  it('formats header', () => {
+    expect(
+      formatMessage({
+        type: 'type',
+        subject: 'subject'
+      })
+    ).toMatchSnapshot()
+  })
+
+  it('formats scoped header', () => {
+    expect(
+      formatMessage({
+        type: 'type',
+        scope: 'scope',
+        subject: 'subject'
+      })
+    ).toMatchSnapshot()
+  })
+
+  it('formats body', () => {
+    expect(
+      formatMessage({
+        type: 'type',
+        scope: 'scope',
+        subject: 'subject',
+        body: ['lore ipsum:', loreipsum]
+      })
+    ).toMatchSnapshot()
+  })
+
+  it('formats breaking change', () => {
+    expect(
+      formatMessage({
+        type: 'type',
+        scope: 'scope',
+        subject: 'subject',
+        breaking: 'a breaking change'
+      })
+    ).toMatchSnapshot()
+  })
+
+  it('formats issues', () => {
+    expect(
+      formatMessage({
+        type: 'fix',
+        scope: 'scope',
+        subject: 'subject',
+        issues: ['#1', '#2']
+      })
+    ).toMatchSnapshot()
+
+    expect(
+      formatMessage({
+        type: 'feat',
+        scope: 'scope',
+        subject: 'subject',
+        issues: ['#1', '#2']
+      })
+    ).toMatchSnapshot()
+  })
+
+  it('formats all', () => {
+    expect(
+      formatMessage({
+        type: 'feat',
+        scope: 'scope',
+        subject: 'subject',
+        body: ['description of the change'],
+        breaking: 'this is a breaking change',
+        issues: ['#1', '#2']
+      })
+    ).toMatchSnapshot()
+  })
+})
