20169df6e6c531b07b38d489583784bf43229cab
stasson <sebastien.tasson@gmail.com>
2021-03-28 16:04:15 +0200

    test(prompt.spec): fix stdin mock timings

---
+++ test/spec/bdd-stdin.js
@@ -0,0 +1,53 @@
+function bddStdin(responses) {
+  if (arguments.length > 1) {
+    responses = Array.prototype.slice.call(arguments, 0)
+  }
+  if (typeof responses === 'string') {
+    responses = [responses]
+  }
+
+  if (!Array.isArray(responses)) {
+    throw new Error(
+      'Expected array of responses, not ' + JSON.stringify(responses, null, 2)
+    )
+  }
+  if (responses.length < 1) {
+    throw new Error(
+      'Expected at least 1 response, not ' + JSON.stringify(responses, null, 2)
+    )
+  }
+
+  var stdin = require('mock-stdin').stdin()
+
+  var k = 0
+
+  function sendAnswer() {
+    setTimeout(function () {
+      var text = responses[k]
+      if (typeof text !== 'string') {
+        throw new Error(
+          'Should give only text responses ' +
+            JSON.stringify(responses, null, 2)
+        )
+      }
+      stdin.send(text)
+      k += 1
+      if (k < responses.length) {
+        sendAnswer()
+      }
+    }, 100)
+  }
+
+  sendAnswer()
+}
+
+bddStdin.keys = Object.freeze({
+  up: '\u001b[A',
+  down: '\u001b[B',
+  left: '\u001b[D',
+  right: '\u001b[C',
+  enter: '\n',
+  space: ' ',
+})
+
+module.exports = bddStdin
