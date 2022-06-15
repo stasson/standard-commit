7fa1d938803b7b54ec4a19a285baf7adc8e2468c
stasson <sebastien.tasson@gmail.com>
2022-06-15 21:09:05 +0200

    test(options.spec): limit snapshot to rules

--- test/spec/options.spec.ts
+++ test/spec/options.spec.ts
@@ -4,97 +4,81 @@ describe('loadOptions', () => {
   it('loads ', async () => {
     expect.hasAssertions()
     const options = await loadOptions()
-    expect(options).toMatchInlineSnapshot(`
+    expect(options.rules).toMatchInlineSnapshot(`
       Object {
-        "defaultIgnores": undefined,
-        "extends": Array [],
-        "formatter": "@commitlint/format",
-        "helpUrl": "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
-        "ignores": undefined,
-        "parserPreset": undefined,
-        "plugins": Object {
-          "localPlugin": Object {
-            "rules": Object {
-              "references-empty-enum": [Function],
-            },
-          },
-        },
-        "prompt": Object {},
-        "rules": Object {
-          "body-leading-blank": Array [
-            1,
-            "always",
+        "body-leading-blank": Array [
+          1,
+          "always",
+        ],
+        "body-max-line-length": Array [
+          2,
+          "always",
+          100,
+        ],
+        "footer-leading-blank": Array [
+          1,
+          "always",
+        ],
+        "footer-max-line-length": Array [
+          2,
+          "always",
+          100,
+        ],
+        "header-max-length": Array [
+          2,
+          "always",
+          100,
+        ],
+        "scope-case": Array [
+          2,
+          "always",
+          "lower-case",
+        ],
+        "scope-empty": Array [
+          2,
+          "always",
+        ],
+        "subject-case": Array [
+          2,
+          "never",
+          Array [
+            "sentence-case",
+            "start-case",
+            "pascal-case",
+            "upper-case",
           ],
-          "body-max-line-length": Array [
-            2,
-            "always",
-            100,
+        ],
+        "subject-empty": Array [
+          2,
+          "never",
+        ],
+        "subject-full-stop": Array [
+          2,
+          "never",
+          ".",
+        ],
+        "type-case": Array [
+          2,
+          "always",
+          "lower-case",
+        ],
+        "type-empty": Array [
+          2,
+          "never",
+        ],
+        "type-enum": Array [
+          2,
+          "always",
+          Array [
+            "feat",
+            "fix",
+            "chore",
+            "docs",
+            "style",
+            "refactor",
+            "test",
           ],
-          "footer-leading-blank": Array [
-            1,
-            "always",
-          ],
-          "footer-max-line-length": Array [
-            2,
-            "always",
-            100,
-          ],
-          "header-max-length": Array [
-            2,
-            "always",
-            100,
-          ],
-          "scope-case": Array [
-            2,
-            "always",
-            "lower-case",
-          ],
-          "scope-empty": Array [
-            2,
-            "always",
-          ],
-          "subject-case": Array [
-            2,
-            "never",
-            Array [
-              "sentence-case",
-              "start-case",
-              "pascal-case",
-              "upper-case",
-            ],
-          ],
-          "subject-empty": Array [
-            2,
-            "never",
-          ],
-          "subject-full-stop": Array [
-            2,
-            "never",
-            ".",
-          ],
-          "type-case": Array [
-            2,
-            "always",
-            "lower-case",
-          ],
-          "type-empty": Array [
-            2,
-            "never",
-          ],
-          "type-enum": Array [
-            2,
-            "always",
-            Array [
-              "feat",
-              "fix",
-              "chore",
-              "docs",
-              "style",
-              "refactor",
-              "test",
-            ],
-          ],
-        },
+        ],
       }
     `)
   })
