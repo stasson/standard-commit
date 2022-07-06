65b0b2b273fa9c3f5d088dab8c5352c0ad7193a9
stasson <sebastien.tasson@gmail.com>
2022-07-06 06:38:02 +0200

    fix(bin): remove update notification

--- bin/commit.js
+++ bin/commit.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 const importLocal = require('import-local');
-const updateNotifier = require('update-notifier');
-const pkg = require('../package.json');
 const loudRejection = require('loud-rejection');
 
 loudRejection(err => console.error('Internal Error:', err.message))
 
-updateNotifier({pkg}).notify();
 if (!importLocal(__filename)) {
   require('../dist/cli/commit.js')
 }
